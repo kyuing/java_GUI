3011a129c8a22bd1286a55092cb4b9372528f9e5
kyu <kyykyu000@gmail.com>
2019-09-09 15:26:02 +0100

    v9: JButton, event and listener, event counter

--- src/_09JButton/JButtonEx.java
+++ src/_09JButton/JButtonEx.java
@@ -64,24 +64,16 @@ public class JButtonEx {
 		 * */
 		jButton.setHorizontalAlignment(SwingConstants.LEFT);	//set a horizontal alignment of jButton by having the public static final int variable LEFT that is in SwingConstants interface class as a parameter. 
 		jButton.setVerticalAlignment(SwingConstants.BOTTOM);	//set a vertical alignment of jButton by having the public static final int variable BOTTOM that is in SwingConstants interface class as a parameter
-//		frame.add(jButton);	//add jButton into frame
+		frame.add(jButton);	//add jButton into frame
 		
 		
 		/**
-		 * The listener interface for receiving action events.
-		 * The class that is interested in processing an action event
-		 * implements this interface, and the object created with that
-		 * class is registered with a component, using the component's
-		 * <code>addActionListener</code> method. When the action event
-		 * occurs, that object's <code>actionPerformed</code> method is
-		 * invoked.
-		 *
-		 * @see ActionEvent
-		 * @see <a href="https://docs.oracle.com/javase/tutorial/uiswing/events/actionlistener.html">How to Write an Action Listener</a>
-		 *
-		 * @author Carl Quinn
-		 * @since 1.1
+		 * The listener interface for receiving action events. 
+		 * The class that is interested in processing an action event implements this interface, and the object created with that class
+		 * is registered with a component, using the component's <code>addActionListener</code> method.
+		 * When the action event occurs, that object's <code>actionPerformed</code> method is invoked.
 		 */
+
 		//create an instance of ActionListener
 		ActionListener listener = new ActionListener() {
 			
@@ -89,27 +81,40 @@ public class JButtonEx {
 			//Invoked when an action occurs.
 		    public void actionPerformed(ActionEvent e) {
 				
-				label.setText(String.valueOf(Integer.valueOf( label.getText() + 1) ) );
-//				label.setText(String.valueOf(Integer.valueOf( label.getText() + 1) ) );
-	
-				clicker++;	//increment it once the button is clicked
-				System.out.println("The button is clicked " + clicker + " time(s)");
+				/*  set a new text value into label whenever action (clicking jButton) occurs.
+				   
+				       the process of a new value works like this; 
+				 ->    old value "1" 
+				 ->    label.setText(String.valueOf(Integer.valueOf( label.getText() + 1) ) ); in public void actionPerformed(ActionEvent e){} in ActionListener listener = new ActionListener(){};   
+				 
+				 ->    label.setText(String.valueOf(Integer.valueOf(label.getText() + 1) ) );	// new value now should be 1 and it's represented as "1"
+				 
+   			 		   after that, the value of the text of label, however, stays at zero or reset to be zero. the method work properly, but it will result in acting like appending a new value which is always 1 on the right side
+				 	   which means, 
+				       1. the initial value zero at JLabel label = new JLabel("0"); could have nothing to do with the method label.setText() overridden in the instance of ActionListener class as the event happens inside the method overridden
+				       2. label.getText() returns the initial value only for the same kind of reason that there is no a getter such as a method label.getText() in the overridden method.           */	
 
+				/* Thus, there are two solution on it. */
+				//way 1: use Math.addExact(x, y) and then get the current value returned from label.getText()
+				label.setText(String.valueOf( Math.addExact(Integer.valueOf(label.getText()), 1) ) );	 //current value + 1
+				System.out.println("The button is being clicked " + label.getText() + " time(s)");
 				
-				label.setText(String.valueOf(clicker));
+				
+				/* way 2: we follow the given condition. we set a class-level private static int variable clicker for counting in Main class. we then increment it INSIDE the overridden method once the overridden method is invoked.
+				   we then set the current value into label by invoking the method label.setText(String.valueOf(clicker));
+				   Now, the text value of label never goes back to its initial one since we separately set a new current value which is being incremented from clicker
+				   In turn, those two label.setText() methods works as they do each with no a scope issue  
+				 
+				label.setText(String.valueOf(Integer.valueOf(label.getText() + 1) ) );	//the original approach given
+				clicker++;	//increment it once the button or correctly speaking, any space in jButton UI is clicked
+				label.setText(String.valueOf(clicker));	//set a new (a literal current value) text from the variable clicker
+				System.out.println("The button is being clicked " + clicker + " time(s)");									*/
 			}
-			
-			
 		};
 		
 		jButton.addActionListener(listener);	//add listener into jButton. call back method approach 
 		
-		
-		
-		
-		
-		
-		frame.add(jButton);	//add jButton into frame
+//		frame.add(jButton);	//add jButton into frame
 		frame.pack();	//modulate the window size of frame
 		frame.setVisible(true);	//true == display.  false == not display
 		
@@ -119,9 +124,7 @@ public class JButtonEx {
 		System.out.println("jButton.getForeground(): " + jButton.getForeground());	//get fore ground
 		System.out.println("jButton.getBackground(): " + jButton.getBackground());	//get back ground
 		System.out.println("jButton.getHorizontalAlignment(): " + jButton.getHorizontalAlignment());	//get horizontal alignment
-		System.out.println("jButton.getVerticalAlignment(): " + jButton.getVerticalAlignment());	//get vertical alignment
-//		System.out.println(clicker);
-		
+		System.out.println("jButton.getVerticalAlignment(): " + jButton.getVerticalAlignment());	//get vertical alignment	
         
 	}
 }
